66d03c54a6e14a7b6ba8cae376b612ff39978d3f
iCharlieAraiza <eliptikman1234@gmail.com>
2022-08-26 21:28:58 -0500

    Add content

---
+++ js/homepage.js
@@ -0,0 +1,12 @@
+var slider = tns({
+    "container": ".carrousel",
+    "items": 2,
+    "mouseDrag": true,
+    "slideBy": "page",
+    "swipeAngle": false,
+    "speed": 400,
+    "loop": false,
+    "controls": false,
+    "rewind	": true,
+    "nav": false,
+  });
