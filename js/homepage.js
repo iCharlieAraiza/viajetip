9036a29181fd104e18f771536547907468e94db0
iCharlieAraiza <eliptikman1234@gmail.com>
2022-09-10 23:23:07 -0500

    Update elements

--- js/homepage.js
+++ js/homepage.js
@@ -10,3 +10,39 @@ var slider = tns({
     "rewind	": true,
     "nav": false,
   });
+
+const cards = document.querySelectorAll('.carousel-cell');
+
+const places = {
+  "hills":{
+    "title":"Zona montañosa, Mianmar",
+    "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl.",
+    "image":"https://images.unsplash.com/photo-1519680772-3b7b1b1b5f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+  },
+  "japan":{
+    "title":"Japan",
+    "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl.",
+    "image":"https://images.unsplash.com/photo-1519680772-3b7b1b1b5f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+  },
+  "maldives":{
+    "title":"Maldives",
+    "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl.",
+    "image":"https://images.unsplash.com/photo-1519680772-3b7b1b1b5f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+  },
+  "venice":{
+    "title":"Venice",
+    "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl.",
+    "image":"https://images.unsplash.com/photo-1519680772-3b7b1b1b5f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+  },
+}
+
+const placeDescription = document.querySelector('.place-name');
+
+
+cards.forEach((card) => {
+  card.addEventListener('click', () => {
+    const value = card.getAttribute('data-value');
+    placeDescription.innerHTML = places[value].title;
+  });
+})
+
