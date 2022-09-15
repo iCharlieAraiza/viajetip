6bbf57889fbd439055cabfa739bf12a6b4d725a9
iCharlieAraiza <eliptikman1234@gmail.com>
2022-09-14 22:11:11 -0500

    update hero slider

--- js/homepage.js
+++ js/homepage.js
@@ -25,7 +25,7 @@ const places = {
     "image":"assets/img/hero/kyoto.jpg"
   },
   "maldives":{
-    "title":"Maldivas",
+    "title":"Maldivas, Océano Índico",
     "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl.",
     "image":"assets/img/hero/maldives-bg.jpg"
   },
@@ -38,12 +38,14 @@ const places = {
 
 const placeDescription = document.querySelector('.place-name');
 const heroBackground = document.querySelector('.hero-background-img');
+const fadein = document.querySelector('.background-fadein');
+
 
 cards.forEach((card) => {
   card.addEventListener('click', () => {
     const value = card.getAttribute('data-value');
     placeDescription.innerHTML = places[value].title;
-    heroBackground.src = places[value].image;
+    fadein.innerHTML = `<img src="${places[value].image}" class="hero-background-img fadein" alt="hero">`
   });
 })
 
