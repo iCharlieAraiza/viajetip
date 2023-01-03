8c3a1cf139b4889b51ef30fb4351bc23f3dcc5a3
iCharlieAraiza <eliptikman1234@gmail.com>
2023-01-02 20:26:04 -0600

    Udpate budgetStyle

---
+++ js/currency.js
@@ -0,0 +1,20 @@
+const currenciesList = []
+
+async function getCurrencies() {
+    const currencies = await fetch('http://www.floatrates.com/daily/usd.json')
+    const json = await currencies.json()
+    for (const key in json) {
+        currenciesList.push({
+            'code': key,
+            'name': json[key].name,
+            'rate': json[key].rate,
+
+        })
+    }
+    //console.log(currenciesList)
+    return currenciesList;
+}
+
+
+
+
